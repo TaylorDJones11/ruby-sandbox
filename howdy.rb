e2740e9fbc3b786ae0b0c744d6aae0008cb1dec7
Taylor D Jones <72883059+TaylorDJones11@users.noreply.github.com>
2024-02-17 00:57:43 +0000

    initial commit

---
+++ howdy.rb
@@ -0,0 +1,12 @@
+# here's a comment 
+my_string = "Hello, world!"
+pp my_string
+
+pp "What's your name?"
+
+their_name = gets.chomp
+
+puts "Hello, " + their_name + "!"
+
+require "./goodbye.rb"
+require "active_support/all"
